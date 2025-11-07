d626381134946dd64955a682b1ea93928c38344e
Miguel-Joaquin <miguel.bsu07@gmail.com>
2025-11-07 19:08:35 +0400

    Create MathQuiz.py

---
+++ MathQuiz.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import random
+
+class MathQuizApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Math Quiz")
+        self.root.geometry("800x600")
+
+        # Load original image
+        self.original_image = Image.open("chalkboard.png")
+        self.bg_image = ImageTk.PhotoImage(self.original_image)
+
+        # Background label
+        self.bg_label = tk.Label(root, image=self.bg_image)
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
