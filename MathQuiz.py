86dbc816190fa2d7139a6d6fdbe1a78974f3455a
Miguel-Joaquin <miguel.bsu07@gmail.com>
2025-11-07 19:36:45 +0400

    Update MathQuiz.py

--- MathQuiz.py
+++ MathQuiz.py
@@ -16,6 +16,19 @@ class MathQuizApp:
         self.bg_label = tk.Label(root, image=self.bg_image)
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # Update background when window resizes
+        self.root.bind("<Configure>", self.resize_bg)
+
+        # Variables
+        self.score = 0
+        self.question_count = 0
+        self.attempt = 1
+        self.difficulty = None  # Will store chosen difficulty
+
+        # Create menu frame (shown first)
+        self.menu_frame = tk.Frame(root, bg="white", bd=5)
+        self.menu_frame.place(relx=0.5, rely=0.5, anchor="center")
+
 
 
 if __name__ == "__main__":
